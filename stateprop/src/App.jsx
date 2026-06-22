ed53b8199371b9b70bb079fc9e6a04fa326a265f
Sameer <sameenaraj01@gmail.com>
2026-06-22 22:58:29 +0530

    added props n states - counter for practise

---
+++ stateprop/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+export default function App(){
+  const [count,setCount]=React.useState(0)
+  function add(){
+    setCount(count+1)
+  }
+  function sub(){
+    setCount(count-1)
+  }
+
+  return(
+    <main className="container">
+      <h1>How many times will Bob say "state" in this section?</h1>
+       <div className="counter">
+        <button className="minus" aria-label="Decrease count" onClick={sub}>-</button>
+        <h2 className="count">{count}</h2>
+        <button className="plus" aria-label="Increase count" onClick={add}>+</button>
+
+       </div>
+    </main>
+  )
+}
